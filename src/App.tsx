7e08dcd0eb8e905c42c806f79f99161f0f977663
sahil <gundechasahil@gmail.com>
2022-02-23 14:47:30 +0530

    dependencies and structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Test from "./Pages/Test";
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reloaddddddd.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Test />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Pages/Test.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Test = () => {
+  return <div>Testing</div>;
+};
+
+export default Test;
