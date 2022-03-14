02669e196eaac937b7422f468a1b443f515940ad
unknown <priyankathapa63@gmail.com>
2022-03-14 18:30:14 +0530

    modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {FC} from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Test from "./Pages/Test";
@@ -19,7 +19,6 @@ import ProfilePage from "./Pages/ProfilePage/ProfilePage";
 import SingleCollectible from "./Pages/SingleCollectible/Singlecollectible";
 import Activity from "./Pages/Activity/Activity";
 import SearchFilter from "./Components/SearchFilterPage/SearchFilter";
-
 import Item from "./Components/Item/Item";
 import BidCarousel from "./Components/CurrentBid/BidCarousel";
 import Discover from "./Pages/DiscoverHomePage/Discover";
@@ -28,7 +27,6 @@ import Transfer from "./Components/Modals/Transfer/Transfer";
 import BurnToken from "./Components/Modals/BurnToken/BurnToken";
 import Report from "./Components/Modals/Report/Report";
 import FooterComponent from "./Components/FooterComponent/Footer";
-
 import FollowModal from "./Components/Modals/FollowModal/FollowModal";
 import NavbarComponent from "./Components/Navbar/NavbarComponent";
 import { AcceptBid } from "./Components/Modals/FollowModal/AcceptBid";
@@ -36,19 +34,16 @@ import EditProfilePage from "./Pages/EditProfilePage/EditProfilePage";
 import { ConnectWallet } from "./Pages/ConnectWallet/ConnectWallet";
 import WalletName from "./Pages/ConnectWallet/WalletName";
 import CreatorsBid from "./Components/CreatorsBid/CreatorsBid";
-
 import SellerBuyerCard from "./Components/SellerBuyerCard/SellerBuyerCard";
 import SellerBuyerCarousel from "./Components/SellerBuyerCard/SellerBuyerCarousel";
 import Crypter from "./Pages/Crypter/Crypter";
-
 import ChangePrice from "./Components/Modals/ChangePrice/ChangePrice";
-
 import CrypterEnd from "./Components/CrypterEnd/CrypterEnd";
 import SingleCollectible1 from "./Pages/SingleCollectible/SingleCollectible1";
 
-const App: React.FC = () => {
+const App: FC = () => {
   return (
-    <div className="xyz">
+    <div>
       <NavbarComponent />
       <Routes>
         <Route path="/" element={<Crypter />} />
@@ -56,42 +51,42 @@ const App: React.FC = () => {
         <Route path="/faq" element={<Faq />} />
         <Route path="/item" element={<Item />} />
 
-        <Route path="/Textfield" element={<Textfield />} />
-        <Route path="UsersCard" element={<UsersCard />} />
-        <Route path="NoResultsPage" element={<NoResultsPage />} />
-        <Route path="/SingleCollectible" element={<SingleCollectible />} />
-        <Route path="searchbar" element={<Searchbar />} />
-        <Route path="carosel" element={<Carosel />} />
-        <Route path="followersCard" element={<FollowersCard />} />
-        <Route path="/NotificationDisplay" element={<NotificationD />} />
-        <Route path="/Upload" element={<Upload />} />
-        <Route path="/CardCollection" element={<CradCollection />} />
-        <Route path="/ProfilePage" element={<ProfilePage />} />
-        <Route path="/Activity" element={<Activity />} />
-        <Route path="/searchfilter" element={<SearchFilter />} />
-        <Route path="/homepage" element={<BidCarousel />} />
-        <Route path="/currentbid" element={<BidCarousel />} />
+        <Route path="/textfield" element={<Textfield />} />
+        <Route path="/users-card" element={<UsersCard />} />
+        <Route path="/no-results-page" element={<NoResultsPage />} />
+        <Route path="/single-collectible" element={<SingleCollectible />} />
+        <Route path="/searchbar" element={<Searchbar />} />
+        <Route path="/carosel" element={<Carosel />} />
+        <Route path="/followers-card" element={<FollowersCard />} />
+        <Route path="/notification-display" element={<NotificationD />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="/card-collection" element={<CradCollection />} />
+        <Route path="/profile-page" element={<ProfilePage />} />
+        <Route path="/activity" element={<Activity />} />
+        <Route path="/search-filter" element={<SearchFilter />} />
+        <Route path="/home-page" element={<BidCarousel />} />
+        <Route path="/current-bid" element={<BidCarousel />} />
         <Route path="/discover" element={<Discover />} />
-        <Route path="WalletName" element={<WalletName />} />
-        <Route path="/Remove" element={<Remove />} />
-        <Route path="/Transfer" element={<Transfer />} />
-        <Route path="/Burn" element={<BurnToken />} />
-        <Route path="/Report" element={<Report />} />
-        <Route path="/Crypter" element={<Crypter />} />
-        <Route path="/ConnectWallet" element={<ConnectWallet />} />
-        <Route path="/CrypterEnd" element={<CrypterEnd />} />
-        <Route path="/Follow" element={<FollowModal />} />
-        <Route path="/Creators" element={<CreatorsBid />} />
-        <Route path="/dropdown" element={<Dropdown item1="test" />} />
+        <Route path="/wallet-name" element={<WalletName />} />
+        <Route path="/remove" element={<Remove />} />
+        <Route path="/transfer" element={<Transfer />} />
+        <Route path="/burn" element={<BurnToken />} />
+        <Route path="/report" element={<Report />} />
+        <Route path="/crypter" element={<Crypter />} />
+        <Route path="/connect-wallet" element={<ConnectWallet />} />
+        <Route path="/crypter-end" element={<CrypterEnd />} />
+        <Route path="/follow" element={<FollowModal />} />
+        <Route path="/creators" element={<CreatorsBid />} />
+        <Route path="/drop-down" element={<Dropdown item1="test" />} />
 
-        <Route path="/Single1" element={<SingleCollectible1 />} />
+        <Route path="/single1" element={<SingleCollectible1 />} />
 
-        <Route path="/SellerBuyerCard" element={<SellerBuyerCard />} />
+        <Route path="/seller-buyer-card" element={<SellerBuyerCard />} />
 
-        <Route path="/ChangePrice" element={<ChangePrice />} />
-        <Route path="/Accept" element={<AcceptBid />} />
-        <Route path="/EditProfilePage" element={<EditProfilePage />} />
-        <Route path="/SellerBuyerCarousel" element={<SellerBuyerCarousel />} />
+        <Route path="/change-price" element={<ChangePrice />} />
+        <Route path="/accept" element={<AcceptBid />} />
+        <Route path="/edit-profile-page" element={<EditProfilePage />} />
+        <Route path="/seller-buyer-carousel" element={<SellerBuyerCarousel />} />
       </Routes>
       <FooterComponent />
     </div>
